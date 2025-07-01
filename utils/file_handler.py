2cafee3b10f7d2417a3ff6ef02c20484df0b9b39
Abdulaziz <abdulaziz.ip33@gmail.com>
2025-07-01 21:50:43 +0300

    draft impl of read files json

---
+++ utils/file_handler.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+def load_json(filepath):
+    if not os.path.exists(filepath):
+        return []
+    
+    try:
+        with open(filepath, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except json.JSONDecodeError:
+        return []
+
+def save_json(filepath, data):
+    with open(filepath, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
