9ccbaa0de4771bf1014de692d7e13444c33a8212
Abdulaziz <abdulaziz.ip33@gmail.com>
2025-07-01 21:46:06 +0300

    first draft of recipe class

--- classes/recipe.py
+++ classes/recipe.py
@@ -1,4 +1,4 @@
-from bean import Bean
+from classes.beans import Beans
 
 
 
@@ -6,16 +6,20 @@ class Recipe:
     GRIND_SIZE_MAX = 30
     GRIND_SIZE_MIN = 1
 
-    def __init__(self, bean: Bean, milk_ml: int, ratio: str, grind_size: int, steps:list):
+    def __init__(self, bean: Beans, grind_coffee_grams:int , milk_ml: int, ratio: str, grind_size: int, steps:list):
         self.set_bean(bean)
         self.set_milk_ml(milk_ml)
         self.set_ratio(ratio)
         self.set_grind_size(grind_size)
-        self.set_steps()
+        self.set_steps(steps)
+        self.set_grind_coffee_grams(grind_coffee_grams)
 
-#Getters
+    def __str__(self):
+        return f"ratio = {self.__ratio}, grind_size = {self.__grind_size}, steps = {self.__steps}, \n(bean = {self.__bean})"
 
-    def get_bean(self) -> Bean:
+    #Getters
+
+    def get_bean(self) -> Beans:
         return self.__bean
 
     def get_milk_ml(self):
@@ -28,14 +32,16 @@ class Recipe:
         return self.__grind_size
     
     def get_steps(self):
-        return self.steps
-
+        return self.__steps
+    
+    def get_grind_coffee_grams(self):
+        return self.__grind_coffee_grams
 
 
 
 #Setters
     def set_bean(self, bean):
-        if isinstance(bean, Bean):
+        if isinstance(bean, Beans):
             self.__bean = bean
         else:
             raise ValueError("bean must be a Bean object")
@@ -57,6 +63,13 @@ class Recipe:
             self.__ratio = ratio
         else:
             raise ValueError("ratio must be a string like \"1:15\"")
+        
+
+    def set_grind_coffee_grams(self, grams):
+        if isinstance(grams, int) and grams>0:
+            self.__grind_coffee_grams=grams
+        else:
+            raise ValueError("grind coffee in grams must be int")
 
 
 
@@ -69,8 +82,33 @@ class Recipe:
             raise ValueError(f"grind_size must be int between {self.GRIND_SIZE_MIN} and {self.GRIND_SIZE_MAX}")
 
 
-    def set_steps(self, steps:list):
+    def set_steps(self, steps: list):
         if isinstance(steps, list):
-            self.__steps= self
+            self.__steps = steps
         else:
-            raise ValueError("steps must be a list ")
+            raise ValueError("steps must be a list")
+
+        
+
+
+#-----
+    def to_dict(self):
+        return {
+            "bean": self.__bean.to_dict(),
+            "grind_coffee_grams": self.__grind_coffee_grams,
+            "milk_ml": self.__milk_ml,
+            "ratio": self.__ratio,
+            "grind_size": self.__grind_size,
+            "steps": self.__steps
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            bean=Beans.from_dict(data["bean"]),
+            grind_coffee_grams=data["grind_coffee_grams"],
+            milk_ml=data["milk_ml"],
+            ratio=data["ratio"],
+            grind_size=data["grind_size"],
+            steps=data["steps"]
+        )
