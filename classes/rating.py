0e2198bb35bfe108921e4ca3eb8a0d0be75e6966
Abdulaziz <abdulaziz.ip33@gmail.com>
2025-07-01 21:46:25 +0300

    first draft of rating class

--- classes/rating.py
+++ classes/rating.py
@@ -4,10 +4,11 @@ from datetime import datetime
 
 class Rating:
 
-    def __init__(self, drink: str, score: int, reviewed_at: datetime = None):
+    def __init__(self, drink: str, score: int,description:str,  reviewed_at: datetime = None):
         self.set_drink(drink)
         self.set_score(score)
         self.set_reviewed_at(reviewed_at)
+        self.set_description(description)
 
 
 
@@ -21,6 +22,10 @@ class Rating:
 
     def get_reviewed_at(self):
         return self.__reviewed_at
+    
+
+    def get_description(self):
+        return self.__description
 
 
 
@@ -37,9 +42,40 @@ class Rating:
             self.__score = score
         else:
             raise ValueError("score must be an int between 1 and 5")
+        
+
+    def set_description(self, description:str):
+        if isinstance(description, str):
+            self.__description= description
+
+        else:
+            raise ValueError("description must be a string")
+
+
+
+
 
     def set_reviewed_at(self, reviewed_at):
         if isinstance(reviewed_at, datetime):
             self.__reviewed_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
         else:
             raise ValueError("reviewed_at must be a datetime object")
+
+
+
+    def to_dict(self):
+        return {
+            "drink": self.__drink,
+            "score": self.__score,
+            "description": self.__description,
+            "reviewed_at": self.__reviewed_at
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            data["drink"],
+            data["score"],
+            data["description"],
+            data["reviewed_at"]
+        )
