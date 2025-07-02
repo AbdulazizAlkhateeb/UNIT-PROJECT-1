26a14abcb1989447d08376b35dafb0f8a2e9bc98
Abdulaziz <abdulaziz.ip33@gmail.com>
2025-07-02 08:01:28 +0300

    Implement Rating class with score, description, and timestamp

--- classes/rating.py
+++ classes/rating.py
@@ -1,19 +1,18 @@
 from datetime import datetime
 
 
-
 class Rating:
 
-    def __init__(self, drink: str, score: int,description:str,  reviewed_at: datetime = None):
+    def __init__(self, drink: str, score: int, description: str, reviewed_at: datetime = None):
         self.set_drink(drink)
         self.set_score(score)
         self.set_reviewed_at(reviewed_at)
         self.set_description(description)
 
+    def __str__(self):
+        return f"drink = {self.__drink}, score = {self.__score}, reviewed_at = {self.__reviewed_at}"
 
-
-
-#Getters
+    # Getters
     def get_drink(self):
         return self.__drink
 
@@ -22,15 +21,14 @@ class Rating:
 
     def get_reviewed_at(self):
         return self.__reviewed_at
-    
 
     def get_description(self):
         return self.__description
 
+    def get_rating_info(self):
+        return f"Score 5/{self.__score} \nReviewed at:({self.__reviewed_at} \nDescription:{self.__description} )"
 
-
-
-#Setters
+    # Setters
     def set_drink(self, drink):
         if isinstance(drink, str):
             self.__drink = drink
@@ -42,24 +40,25 @@ class Rating:
             self.__score = score
         else:
             raise ValueError("score must be an int between 1 and 5")
-        
 
-    def set_description(self, description:str):
+    def set_description(self, description: str):
         if isinstance(description, str):
-            self.__description= description
+            self.__description = description
 
         else:
             raise ValueError("description must be a string")
 
 
-
-
-
     def set_reviewed_at(self, reviewed_at):
-        if isinstance(reviewed_at, datetime):
-            self.__reviewed_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
+        if reviewed_at is None:
+            self.__reviewed_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        elif isinstance(reviewed_at, datetime):
+            self.__reviewed_at = reviewed_at.strftime("%Y-%m-%d %H:%M:%S")
+        elif isinstance(reviewed_at, str):
+            self.__reviewed_at = reviewed_at
         else:
-            raise ValueError("reviewed_at must be a datetime object")
+            raise ValueError("reviewed_at must be a datetime object, a string, or None")
+
 
 
 
@@ -77,5 +76,5 @@ class Rating:
             data["drink"],
             data["score"],
             data["description"],
-            data["reviewed_at"]
+            datetime.strptime(data["reviewed_at"], "%Y-%m-%d %H:%M:%S")
         )
